0f11df4008215ac1e1da3a36c8b4ab191639c8a1
Trevor Newsome <90625539+TrevorNewsome76@users.noreply.github.com>
2024-01-29 19:32:45 +0000

    Modified settings json
    Modified settings json location, and moved code from Sub Main into adapter class. Although this needs refactoring into an extension

--- BlinkCameraCall/BlinkAdapter.cs
+++ BlinkCameraCall/BlinkAdapter.cs
@@ -2,14 +2,69 @@
 using BlinkCommon.Interfaces;
 using Dependency;
 
+using Shadow.Quack;
+
 namespace BlinkCameraCall;
 
 public class BlinkAdapter
 {
+    private static ILoginResponse _loginDetails = null!;
+    private static IBlinkApiTransactions _blinkAdapter =>
+        Shelf.RetrieveInstance<IBlinkApiTransactions>()
+        ?? Duck.Implement<IBlinkApiTransactions>();
+
     public static void Initialize(IBlinkSettings configuration)
     {
         Shelf.Clear();
         Shelf.ShelveInstance(BlinkApiInterface.Initialize(configuration));
         Shelf.ShelveInstance(ApiDriver.HttpClientApiHandler());
+    }
+
+    public static string Login(string[] parameters)
+    {
+        // TODO: Add username and password option for someone who is logging in for the first time
+        _loginDetails = _blinkAdapter?.AuthLogin()?? Duck.Implement<ILoginResponse>(new());
+        _blinkAdapter?.SetAccessToken(_loginDetails?.Auth?.Token ?? string.Empty);
+        return _loginDetails?.Auth?.Token != null
+                                ? "Successfully logged in."
+                                : "FAILED to login.";
+    }
+
+    public static string Logout()
+    {
+        if (_loginDetails is null)
+        {
+            return "Not currently logged in so Logout command was ignored.";
+        }
+
+        if (_loginDetails.Account is null)
+        {
+            return "Account details are missing so Logout command was ignored.";
+        }
+
+        if (_loginDetails.Account.Account_Id == 0 || _loginDetails.Account.Client_Id == 0)
+        {
+            return "Account Id or Client Id are missing so Logout command was ignored.";
+        }
+
+        var logoutResult = _blinkAdapter?.AuthLogout(
+                                   _loginDetails.Account.Account_Id.ToString(),
+                                   _loginDetails.Account.Client_Id.ToString()
+                               )
+                           ?? Duck.Implement<ILogoutResponse>(new());
+
+        return logoutResult?.Message != null ? "Successfully logged out." : "FAILED to logout.";
+    }
+
+    public static string VerifyPin(string[] parameters) {
+        var result = _blinkAdapter?.AuthVerifyPin(
+                                parameters[1],
+                                _loginDetails?.Account?.Tier ?? string.Empty,
+                                _loginDetails?.Account?.Account_Id ?? 0,
+                                _loginDetails?.Account?.Client_Id ?? 0,
+                                _loginDetails?.Auth?.Token ?? string.Empty
+                                );
+
+        return result?.Message != null ? "(" + result.Code.ToString() + ") " + result.Message : "Unknown";
     }
 }
--- BlinkCameraCall/Program.cs
+++ BlinkCameraCall/Program.cs
@@ -8,79 +8,36 @@ namespace BlinkCameraCall
 {
     internal class Program
     {
-        private static ILoginResponse _loginDetails = null!;
-        private static IBlinkApiTransactions BlinkAdapter => 
-            Shelf.RetrieveInstance<IBlinkApiTransactions>() 
-            ?? Duck.Implement<IBlinkApiTransactions>();
 
         static void Main()
         {
             Console.WriteLine("Blink Camera Terminal");
 
             // TODO: Remove hard coded file path. Also what happens if file does not exist. Maybe when login prompt used it will create a settings file
-            var settings = File.ReadAllText(@"F:\repo\BlinkCameraServiceHandler\settings.json").Deserialize<IBlinkSettings>();
+
+            var settings = File.ReadAllText($"{AppContext.BaseDirectory}settings.json").Deserialize<IBlinkSettings>();
 
             Console.SetCursorPosition(0, 1);
 
-            BlinkCameraCall.BlinkAdapter.Initialize(settings);
+            BlinkAdapter.Initialize(settings);
 
             bool quitNow = false;
             while (!quitNow)
             {
                 var command = Console.ReadLine() ?? string.Empty;
-                var splitString = command.Split(' ');
-                switch (splitString[0]?.ToLower() ?? string.Empty)
+                var parameters = command.Split(' ');
+                switch (parameters[0]?.ToLower() ?? string.Empty)
                 {
                     case "login":
-                        _loginDetails = BlinkAdapter?.AuthLogin() 
-                                        ?? Duck.Implement<ILoginResponse>(new());
-
-                        BlinkAdapter?.SetAccessToken(_loginDetails?.Auth?.Token ?? string.Empty);
-                        Console.WriteLine(_loginDetails?.Auth?.Token != null 
-                            ? "Successfully logged in." 
-                            : "FAILED to login.");
-
+                        Console.WriteLine(BlinkAdapter.Login(parameters));
                         break;
 
                     case "logout":
-
-                        if (_loginDetails is null)
-                        {
-                            Console.WriteLine("Not currently logged in so Logout command was ignored.");
-                            break;
-                        }
-
-                        if (_loginDetails.Account is null)
-                        {
-                            Console.WriteLine("Account details are missing so Logout command was ignored.");
-                            break;
-                        }
-
-                        if (_loginDetails.Account.Account_Id == 0 || _loginDetails.Account.Client_Id == 0)
-                        {
-                            Console.WriteLine("Account Id or Client Id are missing so Logout command was ignored.");
-                            break;
-                        }
-                        
-                        var logoutResult = BlinkAdapter?.AuthLogout(
-                                                   _loginDetails.Account.Account_Id.ToString(), 
-                                                   _loginDetails.Account.Client_Id.ToString()
-                                               ) 
-                                           ?? Duck.Implement<ILogoutResponse>(new());
-
-                        Console.WriteLine(logoutResult?.Message != null ? "Successfully logged out." : "FAILED to logout.");
+                        Console.WriteLine(BlinkAdapter.Logout());
                         break;
-
+                        
                     case "pin":
-                        var result = BlinkAdapter?.AuthVerifyPin(
-                                splitString[1], 
-                                _loginDetails?.Account?.Tier ?? string.Empty, 
-                                _loginDetails?.Account?.Account_Id ?? 0, 
-                                _loginDetails?.Account?.Client_Id ?? 0, 
-                                _loginDetails?.Auth?.Token ?? string.Empty
-                                );
-
-                        Console.WriteLine(result?.Message != null ? "(" + result.Code.ToString() + ") " + result.Message: "Unknown");
+                        Console.WriteLine(BlinkAdapter.VerifyPin(parameters));
                         break;
                     case "quit": 
                     case "exit":
